37e48380e420f97a039eda5e39805dce1667c7e4
Gustavo Mews <gustavofariasmews@gmail.com>
2026-04-06 09:27:48 -0300

    icon

--- scripts/copy-assets.mjs
+++ scripts/copy-assets.mjs
@@ -8,6 +8,7 @@ const distDir = resolve(root, "dist");
 mkdirSync(distDir, { recursive: true });
 cpSync(resolve(srcDir, "index.html"), resolve(distDir, "index.html"));
 cpSync(resolve(srcDir, "css"), resolve(distDir, "css"), { recursive: true });
+cpSync(resolve(srcDir, "favicon.svg"), resolve(distDir, "favicon.svg"));
 
 if (existsSync(resolve(root, ".env"))) {
   cpSync(resolve(root, ".env"), resolve(distDir, ".env"));
